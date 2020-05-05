1acfaef5587fb80526e420406228469c8d5077e6
zdunker <woshilisheng@gmail.com>
2020-05-05 14:59:25 -0400

    register app to admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('student/', include('student.urls')),
     path('admin/', admin.site.urls),
-    path('student/', include('student.urls'))
 ]
